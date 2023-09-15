d92dd15e1242cfbee9c4dc093f87faa2f7856523
Michael Vodyanyy <leequidman@gmail.com>
2023-09-15 19:11:56 +0300

    Playing with broadcasting

--- GameClient/Program.cs
+++ GameClient/Program.cs
@@ -9,29 +9,96 @@ namespace GameClient
         {
             Console.WriteLine("Hello, World!");
 
+
             var ws = new ClientWebSocket();
+            string name;
+            while (true)
+            {
+                Console.Write("Input name: ");
+                name = Console.ReadLine();
+                break;
+            }
+
             Console.WriteLine("Start connecting...");
-            await ws.ConnectAsync(new Uri("ws://localhost:13371/ws"), CancellationToken.None);
+            await ws.ConnectAsync(new Uri($"ws://localhost:13371/ws?name={name}"), CancellationToken.None);
             Console.WriteLine("Connected");
+
 
             var receiveTask = Task.Run(async () =>
             {
-                var buffer = new byte[1024];
+                var buffer = new byte[1024 * 4];
                 while (true)
                 {
                     var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
                     if (result.MessageType == WebSocketMessageType.Close)
                     {
-                        Console.WriteLine("Received close message");
                         break;
                     }
 
                     var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                    Console.WriteLine("Received: " + message);
+                    Console.WriteLine(message);
                 }
             });
 
-            await receiveTask;
+            var sendTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var message = Console.ReadLine();
+
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+
+                    var bytes = Encoding.UTF8.GetBytes(message);
+                    await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            });
+
+            await Task.WhenAny(sendTask, receiveTask);
+
+            Console.WriteLine("After when any");
+
+
+            if (ws.State != WebSocketState.Closed)
+            {
+                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+            }
+
+
+            await Task.WhenAll(sendTask, receiveTask);
+
+
+
+
+
+
+
+            // var ws = new ClientWebSocket();
+            // Console.WriteLine("Start connecting...");
+            // await ws.ConnectAsync(new Uri("ws://localhost:13371/ws"), CancellationToken.None);
+            // Console.WriteLine("Connected");
+            //
+            // var receiveTask = Task.Run(async () =>
+            // {
+            //     var buffer = new byte[1024];
+            //     while (true)
+            //     {
+            //         var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            //         if (result.MessageType == WebSocketMessageType.Close)
+            //         {
+            //             Console.WriteLine("Received close message");
+            //             break;
+            //         }
+            //
+            //         var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            //         Console.WriteLine("Received: " + message);
+            //     }
+            // });
+            //
+            // await receiveTask;
         }
     }
 }
--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -14,36 +14,97 @@ namespace GameServer
 
             app.UseWebSockets();
 
-            app.Map("/ws",
-                async context =>
+            var connections = new List<WebSocket>();
+
+            app.Map("/ws", async context =>
                 {
                     if (context.WebSockets.IsWebSocketRequest)
                     {
                         using var ws = await context.WebSockets.AcceptWebSocketAsync();
+                        connections.Add(ws);
 
-                        while (true)
+                        var curName = context.Request.Query["name"];
+                        await Broadcast($"{curName} joined the room");
+                        await Broadcast($"{connections.Count} users connected");
+
+                        await ReceiveMessage(ws,
+                            async (result, buffer) =>
+                            {
+                                if (result.MessageType == WebSocketMessageType.Text)
+                                {
+                                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                                    await Broadcast(curName + ": " + message);
+                                }
+                                else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+                                {
+                                    connections.Remove(ws);
+                                    await Broadcast($"{curName} left the room");
+                                    await Broadcast($"{connections.Count} users connected");
+                                    await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                                }
+                            });
+
+                        
+
+                        async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
                         {
-                            var message = "Time now is: " + DateTime.Now.ToString("HH:mm:ss");
-                            var bytes = Encoding.UTF8.GetBytes(message);
-                            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-                            if (ws.State == WebSocketState.Open)
+                            var buffer = new byte[1024 * 4];
+                            while (socket.State == WebSocketState.Open)
                             {
-                                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+                                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                                handleMessage(result, buffer);
                             }
-                            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
-                            {
-                                break;
-                            }
-
-                            await Task.Delay(1000);
                         }
+
+                        async Task Broadcast(string message)
+                        {
+                            var bytes = Encoding.UTF8.GetBytes(message);
+                            foreach (var socket in connections)
+                            {
+                                if (socket.State == WebSocketState.Open)
+                                {
+                                    var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+                                    await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+                                }
+                            }
+                        }
+
                     }
                     else
                     {
                         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                     }
                 });
+
+
             await app.RunAsync();
+
+
         }
     }
-}
+
+
+}//     // //     while (true)
+//     // //     {
+//     // //         var message = "Time now is: " + DateTime.Now.ToString("HH:mm:ss");
+//     // //         var bytes = Encoding.UTF8.GetBytes(message);
+//     // //         var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+//     // //         if (ws.State == WebSocketState.Open)
+//     // //         {
+//     // //             Console.WriteLine("Sending this: " + message);
+//     // //             await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+//     // //         }
+//     // //         else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+//     // //         {
+//     // //             Console.WriteLine("Websocket closed. By client?");  
+//     // //             break;
+//     // //         }
+//     // //
+//     // //         await Task.Delay(1000);
+//     // //     }
+//     // // }
+//     // else
+//     // {
+//     //     context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+//     // }
+// });
