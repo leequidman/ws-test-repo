9c51be04d9909050d342a19db1ff0e3bce816980
Michael Vodyanyy <leequidman@gmail.com>
2023-09-15 18:26:39 +0300

    Create WS handling in server placeholder

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -1,15 +1,49 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
 namespace GameServer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.UseUrls("http://localhost:1337");
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseWebSockets();
 
-            app.Run();
+            app.Map("/ws",
+                async context =>
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+
+                        while (true)
+                        {
+                            var message = "Time now is: " + DateTime.Now.ToString("HH:mm:ss");
+                            var bytes = Encoding.UTF8.GetBytes(message);
+                            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+                            if (ws.State == WebSocketState.Open)
+                            {
+                                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+                            }
+                            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+                            {
+                                break;
+                            }
+
+                            await Task.Delay(1000);
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    }
+                });
+            await app.RunAsync();
         }
     }
 }
